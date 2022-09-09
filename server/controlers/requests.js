25cd574e17fac10251b5723e1b74f45bdc14cfd4
Kanem <kanemdeng@email.com>
2022-09-09 00:14:00 -0700

    connected mongodb with express

---
+++ server/controlers/requests.js
@@ -0,0 +1,5 @@
+
+export const getRequest = async (req, res) => {
+  res.send('hello world')
+}
+
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import routes from './routes/routes.js'
+
+const app = express()
+dotenv.config()
+
+const URL = process.env.HOST_MONGODB
+const PORT = process.env.PORT || 4000
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('connected to mongoDB')
+    app.listen(PORT, () => {
+      console.log('listening server')
+    })
+  })
+  .catch(error => console.log(error.message))
+
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use('/', routes)
